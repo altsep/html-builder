ac87c2a9af31328c445892bb90dac0ff066b3bf5
Alexei Arushanov <mx24xnpi4@mozmail.com>
2022-11-08 19:57:23 +0300

    fix: create separate writeStream for each file in task 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,7 +5,7 @@ const stream = require('stream');
 const myTransform = new stream.Transform({
   transform(chunk, encoding, callback) {
     /* remove spaces */
-    const data = chunk.toString().replace(/(?<=,|{|;|}|\))\s+/g, '');
+    // const data = chunk.toString().replace(/(?<=,|{|;|}|\))\s+/g, '');
     callback(null, /* data */ chunk);
   },
 });
@@ -14,16 +14,16 @@ const STYLES_DIR = path.join(__dirname, 'styles');
 
 fs.readdir(STYLES_DIR, { withFileTypes: true }, (err, list) => {
   if (err) throw err;
-  const OUTPUT = path.join(__dirname, 'project-dist', 'bundle.css');
-  const writeStream = fs.createWriteStream(OUTPUT);
   list.forEach((item) => {
     const ext = path.extname(item.name);
     if (item.isDirectory() || ext !== '.css') return;
     const FILE_PATH = path.join(STYLES_DIR, item.name);
     const readStream = fs.createReadStream(FILE_PATH, { encoding: 'utf8' });
-    // readStream.pipe(myTransform).pipe(writeStream);
-    stream.pipeline(readStream, writeStream, (err) => {
-      if (err) throw err;
-    });
+    const OUTPUT_PATH = path.join(__dirname, 'project-dist', 'bundle.css');
+    const writeStream = fs.createWriteStream(OUTPUT_PATH);
+    readStream.pipe(myTransform).pipe(writeStream);
+    // stream.pipeline(readStream, writeStream, (err) => {
+    //   if (err) throw err;
+    // });
   });
 });
